b886895cf11b8438928a103511f869ee1b90c7fb
delta <deltaniu_mx@163.com>
2025-12-12 13:00:13 +0800

    feat: add reactive store creation and consumption hooks for state management.

---
+++ lua/plugins/theme/tokyonight.lua
@@ -0,0 +1,11 @@
+return {
+  -- Theme
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+}
