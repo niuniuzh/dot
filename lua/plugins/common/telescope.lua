ff024c27ea32eaf5ddb91522be8b9a72b0af3904
delta <deltaniu_mx@163.com>
2025-12-12 15:58:57 +0800

    feat: Introduce `useChat` hook and integrate it into the `Chat` component with a `conversationId` prop.

---
+++ lua/plugins/common/telescope.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
+
+      vim.keymap.set("n", "<leader>pws", function()
+        local word = vim.fn.expand("<cword>")
+        builtin.grep_string({ search = word })
+      end, { desc = "Search Word Under Cursor" })
+
+      vim.keymap.set("n", "<leader>pWs", function()
+        local word = vim.fn.expand("<cWORD>")
+        builtin.grep_string({ search = word })
+      end, { desc = "Search WORD Under Cursor" })
+    end,
+  },
+}
